79f03def3283b2a8ce376212f6ae59fd71d36ddb
kosmos434 <kosmos434@protonmail.com>
2019-12-19 15:17:21 -0600

    solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -2,14 +2,12 @@ class Book
 # write your code here
     attr_accessor :title
 
-
     def title
-        newStr = @title.split()
+        newStr = @title.capitalize.split()
         conjunctions = ["a", "an", "and", "in", "of", "the"]
-        # First word always capped (even "the")
-        newStr[0].capitalize!
-        # not in ["an", "the", "a", "in"]
+        # Capitalize non conjunctions
         newStr.map! { |x| conjunctions.include?(x) ? x : x.capitalize }
         newStr.join(" ")
     end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+
+  # Get and set @seconds
+  attr_accessor :seconds
+  
+  # Initialize a default value of zero
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    Time.at(@seconds).gmtime.strftime('%R:%S')
+  end
+
 end
